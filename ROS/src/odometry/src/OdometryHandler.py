6744e36d8a156a24c6ffccf86e0e368302c844bd
ongaroleandro <79772565+ongaroleandro@users.noreply.github.com>
2021-06-07 23:47:20 +0200

    Cleaned up code

--- ROS/src/odometry/src/OdometryHandler.py
+++ ROS/src/odometry/src/OdometryHandler.py
@@ -5,7 +5,6 @@ import numpy as np
 import geometry_msgs.msg
 from std_msgs.msg import Float32MultiArray
 from nav_msgs.msg import Odometry
-from matplotlib import pyplot as plt
 import tf_conversions
 import tf2_ros
 
@@ -17,19 +16,6 @@ class OdometryHandler(object):
 	def __init__(self):
 		self.arduino_data_processor = DataProcessor("arduino_data", 0.025, 0.210)
 		self.pub = Publisher('odom', "odom", "base_link")
-
-	def simulate(self,sim_data):
-		for data in sim_data:
-			wl, wr, t_ard = self.arduino_data_processor.extractData(data)
-
-			self.arduino_data_processor.calcPoseTwist(wl, wr, t_ard)
-
-			x_y_theta_t, vx_vth = self.arduino_data_processor.getPublisherInfo()
-
-			msg = self.pub.createNavMsg(1200, x_y_theta_t, vx_vth)
-
-			plt.plot(msg.pose.pose.position.x, msg.pose.pose.position.y, 'b.')
-			#print(self.pub.createNavMsg(1200, x_y_theta_t, vx_vth))
 
 	def main(self):
 		current_time = self.pub.getCurrentTime()
@@ -53,46 +39,3 @@ def startNode():
 
 if __name__ == "__main__":
 	startNode()
-
-'''
-r_wheel = 0.025
-wheel_sep = 0.210
-
-sim_vel = [[10, 10, 1],
-           [10, 10, 2],
-           [10, 10, 3],
-           [10, 10, 4],
-           [-(wheel_sep*0.5*np.pi)/(2*r_wheel), (wheel_sep*0.5*np.pi)/(2*r_wheel), 5],
-           [10, 10, 6],
-           [10, 10, 7],
-           [10, 10, 8],
-           [10, 10, 9],
-           [-(wheel_sep*0.5*np.pi)/(2*r_wheel), (wheel_sep*0.5*np.pi)/(2*r_wheel), 10],
-           [10, 10, 11],
-           [10, 10, 12],
-           [10, 10, 13],
-           [10, 10, 14]]
-
-
-def giveData():
-	wl = 5
-	wr = 10
-
-	simulated_data = []
-	delta_t = 0.1
-	i = 0
-	while i < 10:
-		simulated_data.append([wl, wr, i])
-		i += delta_t
-
-	return simulated_data
-
-
-fig = plt.figure()
-odomhandler = OdometryHandler()
-odomhandler.simulate(giveData())
-plt.show()
-
-
-#print(odomhandler.arduino_data_processor.x_y_theta_t)
-'''
