f34f7e6c32d44ad65b4b5d83c85fba73a5c004f5
Leandro Ongaro <ongaro.leandro@gmail.com>
2021-07-01 12:08:55 +0200

    Fixed indentation

--- ROS/src/odometry/src/dataprocessor.py
+++ ROS/src/odometry/src/dataprocessor.py
@@ -11,84 +11,84 @@ import math
 
 class DataProcessor(object):
 
-	def __init__(self, topic_name, r_wheel, wheel_sep):
+    def __init__(self, topic_name, r_wheel, wheel_sep):
 
-		self.topic_name = topic_name
+        self.topic_name = topic_name
 
-		self.r_wheel = r_wheel
-		self.wheel_sep = wheel_sep
-		self.x_y_theta_t = np.array([0, 0, 0, 0])
-		self.vx_vth = [0,0]
+        self.r_wheel = r_wheel
+        self.wheel_sep = wheel_sep
+        self.x_y_theta_t = np.array([0, 0, 0, 0])
+        self.vx_vth = [0,0]
 
-		self._createSubscriber()
+        self._createSubscriber()
 
-	def _createSubscriber(self):
-	    #wait for message at initialisation before processing data	
-            self.arduino_data = None
-            while self.arduino_data is None and not rospy.is_shutdown():
-                try:
-                    self.arduino_data = rospy.wait_for_message(self.topic_name, Float32MultiArray, timeout=1.0) #define how long we want to wait for a message
-                except:
-                    rospy.logerr("error in foo") #print error screen after waiting and not receiving a message
-            self.processData(self.arduino_data) #calculate twist and pose from odometry information
-            self.sub = rospy.Subscriber(self.topic_name, Float32MultiArray, self.processData)
+    def _createSubscriber(self):
+        #wait for message at initialisation before processing data  
+        self.arduino_data = None
+        while self.arduino_data is None and not rospy.is_shutdown():
+            try:
+                self.arduino_data = rospy.wait_for_message(self.topic_name, Float32MultiArray, timeout=1.0) #define how long we want to wait for a message
+            except:
+                rospy.logerr("error in foo") #print error screen after waiting and not receiving a message
+        self.processData(self.arduino_data) #calculate twist and pose from odometry information
+        self.sub = rospy.Subscriber(self.topic_name, Float32MultiArray, self.processData)
 
-	def processData(self, msg):
-		self.wl, self.wr, self.t_ard = self.extractData(msg)  #TODO: create data variable instead of wl, wr and t_ard
+    def processData(self, msg):
+        self.wl, self.wr, self.t_ard = self.extractData(msg)  #TODO: create data variable instead of wl, wr and t_ard
 
-	def extractData(self, msg):
+    def extractData(self, msg):
 
-		wl = msg.data[0]*np.pi/180
-		wr = msg.data[1]*np.pi/180
-		t_ard = msg.data[2]
+        wl = msg.data[0]*np.pi/180
+        wr = msg.data[1]*np.pi/180
+        t_ard = msg.data[2]
 
-		return wl, wr, t_ard
+        return wl, wr, t_ard
 
-	def calcPoseTwist(self, wl, wr, t_ard):
-		if ((self.x_y_theta_t == [0, 0, 0, 0]).all()):
-			self.x_y_theta_t = np.array([0, 0, 0, t_ard])
+    def calcPoseTwist(self, wl, wr, t_ard):
+        if ((self.x_y_theta_t == [0, 0, 0, 0]).all()):
+            self.x_y_theta_t = np.array([0, 0, 0, t_ard])
 
-		delta_theta = (- wl * self.r_wheel * (t_ard - self.x_y_theta_t[3]) + wr * self.r_wheel * (t_ard - self.x_y_theta_t[3])) / self.wheel_sep
-		delta_s = (wl * self.r_wheel * (t_ard - self.x_y_theta_t[3]) + wr * self.r_wheel * (t_ard - self.x_y_theta_t[3])) * 0.5
-		
-		x = self.x_y_theta_t[0] + delta_s * np.cos(self.x_y_theta_t[2] * np.pi / 180 + delta_theta * 0.5)		
-		y = self.x_y_theta_t[1] + delta_s * np.sin(self.x_y_theta_t[2] * np.pi / 180 + delta_theta * 0.5)
-		theta = self.x_y_theta_t[2] + delta_theta * 180 / np.pi 
-		if theta >= 360:
-			theta = theta - (360 * math.floor(theta / 360))
-		elif theta <= -360:
-			theta = abs(theta)
-			theta = -(theta - (360 * math.floor(theta / 360)))
-		
-		self.x_y_theta_t = np.array([x, y, theta, t_ard])
-
-
-		#twist calculation
-		if (wl > 0 and wr > 0):
-			if (wl > wr):
-				self.vx_vth[0] = wr * self.r_wheel
-			else:
-				self.vx_vth[0] = wl * self.r_wheel
-			self.vx_vth[1] = -self.r_wheel * (wl - wr) / (0.5 * self.wheel_sep)
-		elif (wl < 0 and wr < 0):
-			if (wl < wr):
-				self.vx_vth[0] = wr*self.r_wheel
-			else:
-				self.vx_vth[0] = wl*self.r_wheel
-			self.vx_vth[1] = -self.r_wheel * (wl - wr) / (0.5 * self.wheel_sep)
-		else:
-			self.vx_vth[0] = 0
-			self.vx_vth[1] = self.r_wheel * wr / (0.5 * self.wheel_sep)
+        delta_theta = (- wl * self.r_wheel * (t_ard - self.x_y_theta_t[3]) + wr * self.r_wheel * (t_ard - self.x_y_theta_t[3])) / self.wheel_sep
+        delta_s = (wl * self.r_wheel * (t_ard - self.x_y_theta_t[3]) + wr * self.r_wheel * (t_ard - self.x_y_theta_t[3])) * 0.5
+        
+        x = self.x_y_theta_t[0] + delta_s * np.cos(self.x_y_theta_t[2] * np.pi / 180 + delta_theta * 0.5)       
+        y = self.x_y_theta_t[1] + delta_s * np.sin(self.x_y_theta_t[2] * np.pi / 180 + delta_theta * 0.5)
+        theta = self.x_y_theta_t[2] + delta_theta * 180 / np.pi 
+        if theta >= 360:
+            theta = theta - (360 * math.floor(theta / 360))
+        elif theta <= -360:
+            theta = abs(theta)
+            theta = -(theta - (360 * math.floor(theta / 360)))
+        
+        self.x_y_theta_t = np.array([x, y, theta, t_ard])
 
 
-		return True
-	
-	def getBroadcasterInfo(self):
-		self.calcPoseTwist(self.wl, self.wr, self.t_ard)
+        #twist calculation
+        if (wl > 0 and wr > 0):
+            if (wl > wr):
+                self.vx_vth[0] = wr * self.r_wheel
+            else:
+                self.vx_vth[0] = wl * self.r_wheel
+            self.vx_vth[1] = -self.r_wheel * (wl - wr) / (0.5 * self.wheel_sep)
+        elif (wl < 0 and wr < 0):
+            if (wl < wr):
+                self.vx_vth[0] = wr*self.r_wheel
+            else:
+                self.vx_vth[0] = wl*self.r_wheel
+            self.vx_vth[1] = -self.r_wheel * (wl - wr) / (0.5 * self.wheel_sep)
+        else:
+            self.vx_vth[0] = 0
+            self.vx_vth[1] = self.r_wheel * wr / (0.5 * self.wheel_sep)
 
-		return self.x_y_theta_t
 
-	def getPublisherInfo(self):
-		self.calcPoseTwist(self.wl, self.wr, self.t_ard)
+        return True
+    
+    def getBroadcasterInfo(self):
+        self.calcPoseTwist(self.wl, self.wr, self.t_ard)
 
-		return self.x_y_theta_t, self.vx_vth
+        return self.x_y_theta_t
+
+    def getPublisherInfo(self):
+        self.calcPoseTwist(self.wl, self.wr, self.t_ard)
+
+        return self.x_y_theta_t, self.vx_vth
--- ROS/src/odometry/src/publisher.py
+++ ROS/src/odometry/src/publisher.py
@@ -10,63 +10,63 @@ import tf2_ros
 
 class Publisher(object):
 
-	def __init__(self, topic_name, frame_id, child_frame_id):
+    def __init__(self, topic_name, frame_id, child_frame_id):
 
-		self.frame_id = frame_id
-		self.child_frame_id = child_frame_id
+        self.frame_id = frame_id
+        self.child_frame_id = child_frame_id
 
-		self.topic_name = topic_name
-		self.odom_pub = rospy.Publisher(self.topic_name, Odometry, queue_size=1)
-		self.odom_broadcaster = tf2_ros.TransformBroadcaster()
+        self.topic_name = topic_name
+        self.odom_pub = rospy.Publisher(self.topic_name, Odometry, queue_size=1)
+        self.odom_broadcaster = tf2_ros.TransformBroadcaster()
 
-	def getCurrentTime(self):
-		return rospy.Time.now()
+    def getCurrentTime(self):
+        return rospy.Time.now()
 
-	def createTF(self, current_time, x_y_theta_t):
-		odom_trans = geometry_msgs.msg.TransformStamped()
+    def createTF(self, current_time, x_y_theta_t):
+        odom_trans = geometry_msgs.msg.TransformStamped()
 
-		odom_trans.header.stamp = current_time
-		odom_trans.header.frame_id = self.frame_id
-		odom_trans.child_frame_id = self.child_frame_id				
-		odom_trans.transform.translation.x = x_y_theta_t[0]
-		odom_trans.transform.translation.y = x_y_theta_t[1]
-		odom_trans.transform.translation.z = 0.0
-		
-		q = tf_conversions.transformations.quaternion_from_euler(0, 0, x_y_theta_t[2]*np.pi/180)
-		
-		odom_trans.transform.rotation.x = q[0]
-		odom_trans.transform.rotation.y = q[1]
-		odom_trans.transform.rotation.z = q[2]
-		odom_trans.transform.rotation.w = q[3]
+        odom_trans.header.stamp = current_time
+        odom_trans.header.frame_id = self.frame_id
+        odom_trans.child_frame_id = self.child_frame_id             
+        odom_trans.transform.translation.x = x_y_theta_t[0]
+        odom_trans.transform.translation.y = x_y_theta_t[1]
+        odom_trans.transform.translation.z = 0.0
+        
+        q = tf_conversions.transformations.quaternion_from_euler(0, 0, x_y_theta_t[2]*np.pi/180)
+        
+        odom_trans.transform.rotation.x = q[0]
+        odom_trans.transform.rotation.y = q[1]
+        odom_trans.transform.rotation.z = q[2]
+        odom_trans.transform.rotation.w = q[3]
 
-		return odom_trans
+        return odom_trans
 
-	def createNavMsg(self, current_time, x_y_theta_t, vx_vth):
-		odom = Odometry()
+    def createNavMsg(self, current_time, x_y_theta_t, vx_vth):
+        odom = Odometry()
 
-		odom.header.stamp = current_time	
-		odom.header.frame_id = self.frame_id
-		odom.pose.pose.position.x = x_y_theta_t[0]
-		odom.pose.pose.position.y = x_y_theta_t[1]
-		odom.pose.pose.position.z = 0.0
+        odom.header.stamp = current_time    
+        odom.header.frame_id = self.frame_id
+        odom.pose.pose.position.x = x_y_theta_t[0]
+        odom.pose.pose.position.y = x_y_theta_t[1]
+        odom.pose.pose.position.z = 0.0
 
-		q = tf_conversions.transformations.quaternion_from_euler(0, 0, x_y_theta_t[2]*np.pi/180)
+        q = tf_conversions.transformations.quaternion_from_euler(0, 0, x_y_theta_t[2]*np.pi/180)
 
-		odom.pose.pose.orientation.x = q[0]
-		odom.pose.pose.orientation.y = q[1]
-		odom.pose.pose.orientation.z = q[2]
-		odom.pose.pose.orientation.w = q[3]
+        odom.pose.pose.orientation.x = q[0]
+        odom.pose.pose.orientation.y = q[1]
+        odom.pose.pose.orientation.z = q[2]
+        odom.pose.pose.orientation.w = q[3]
 
-		odom.child_frame_id = self.child_frame_id
-		odom.twist.twist.linear.x = vx_vth[0]
-		odom.twist.twist.linear.y = 0	
-		odom.twist.twist.linear.z = 0
-		odom.twist.twist.angular.x = 0
-		odom.twist.twist.angular.y = 0
-		odom.twist.twist.angular.z = vx_vth[1]
+        odom.child_frame_id = self.child_frame_id
+        odom.twist.twist.linear.x = vx_vth[0]
+        odom.twist.twist.linear.y = 0   
+        odom.twist.twist.linear.z = 0
+        odom.twist.twist.angular.x = 0
+        odom.twist.twist.angular.y = 0
+        odom.twist.twist.angular.z = vx_vth[1]
 
-		return odom
+        return odom
 
-	def publishMessage(self, odom, odom_trans):
-		self.odom_broadcaster.sendTransform(odom_trans)
-		self.odom_pub.publish(odom)
+    def publishMessage(self, odom, odom_trans):
+        self.odom_broadcaster.sendTransform(odom_trans)
+        self.odom_pub.publish(odom)
